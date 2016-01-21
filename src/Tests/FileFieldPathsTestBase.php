fadb568e65642a2a823ffc10d7a8a0fa5820ac4b
Stuart Clark <stu@rtclark.net>
2016-01-22 10:02:50 +1100

    Updated tests for Pathauto alpha.

--- src/Tests/FileFieldPathsTestBase.php
+++ src/Tests/FileFieldPathsTestBase.php
@@ -93,6 +93,7 @@ abstract class FileFieldPathsTestBase extends FileFieldTestBase {
       ->save();
 
     $this->drupalPostForm("admin/structure/types/manage/{$this->contentType}/fields/node.{$this->contentType}.{$name}", [], $this->t('Save settings'));
+    $this->assertText($this->t('Saved @name configuration', ['@name' => $name]));
 
     // Clear field cache in order to avoid stale cache values.
     \Drupal::entityManager()->clearCachedFieldDefinitions();
