aacec561d09639c52f6b7483b664d93e2e0c1814
Oleh Vehera <44159-voleger@users.noreply.drupalcode.org>
2025-12-08 17:35:55 +0000

    feat: #3562058 Convert filefield_paths_local_tasks_alter to OOP

---
+++ src/Hook/LocalTaskAlter.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\filefield_paths\Hook;
+
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Local task alter hook implementation.
+ */
+final class LocalTaskAlter {
+
+  use StringTranslationTrait;
+
+  /**
+   * Implements hook_local_tasks_alter().
+   */
+  #[Hook('local_tasks_alter')]
+  public function localTasksAlterImplementation(array &$local_tasks): void {
+    foreach ($local_tasks as $definition) {
+      if ($definition['route_name'] === 'system.file_system_settings') {
+        // Filesystem route exists - no need to add our own.
+        return;
+      }
+    }
+    // Provide filesystem route if it not exists.
+    $local_tasks['system.file_system_settings'] = [
+        'route_name' => 'system.file_system_settings',
+        'base_route' => 'system.file_system_settings',
+        'title' => $this->t('Settings'),
+        'id' => 'system.file_system_settings',
+      ] + $local_tasks['filefield_paths.admin_settings'];
+  }
+
+}
