c1a733d29fc81fe6840f8384fd5e20776a7f0d6d
Oleh Vehera <44159-voleger@users.noreply.drupalcode.org>
2025-12-08 23:01:08 +0000

    feat: #3562058 Convert filefield_paths_local_tasks_alter to OOP

--- src/Hook/LocalTaskAlter.php
+++ src/Hook/LocalTaskAlter.php
@@ -15,8 +15,9 @@ final class LocalTaskAlter {
   /**
    * Implements hook_local_tasks_alter().
    */
+  // @phpstan-ignore-next-line
   #[Hook('local_tasks_alter')]
-  public function localTasksAlterImplementation(array &$local_tasks): void {
+  public function localTasksAlterImplementation(array &$local_tasks): void {// phpcs:ignore Squiz.WhiteSpace.FunctionSpacing.Before
     foreach ($local_tasks as $definition) {
       if ($definition['route_name'] === 'system.file_system_settings') {
         // Filesystem route exists - no need to add our own.
@@ -25,11 +26,11 @@ final class LocalTaskAlter {
     }
     // Provide filesystem route if it not exists.
     $local_tasks['system.file_system_settings'] = [
-        'route_name' => 'system.file_system_settings',
-        'base_route' => 'system.file_system_settings',
-        'title' => $this->t('Settings'),
-        'id' => 'system.file_system_settings',
-      ] + $local_tasks['filefield_paths.admin_settings'];
+      'route_name' => 'system.file_system_settings',
+      'base_route' => 'system.file_system_settings',
+      'title' => $this->t('Settings'),
+      'id' => 'system.file_system_settings',
+    ] + $local_tasks['filefield_paths.admin_settings'];
   }
 
 }
