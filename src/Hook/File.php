ec42e717996d78409a101a8105312167400ba961
Oleh Vehera <44159-voleger@users.noreply.drupalcode.org>
2025-12-09 02:07:30 +0200

    feat: #3562124 Convert filefield_paths_entity_base_field_info to OOP
    By: voleger

--- src/Hook/File.php
+++ src/Hook/File.php
@@ -2,13 +2,34 @@
 
 namespace Drupal\filefield_paths\Hook;
 
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\file\FileInterface;
 
 /**
  * File relate hook implementations.
  */
 final class File {
+
+  use StringTranslationTrait;
+
+  /**
+   * Implements hook_entity_base_field_info().
+   */
+  // @phpstan-ignore-next-line
+  #[Hook('entity_base_field_info')]
+  public function entityBaseFieldInfo(EntityTypeInterface $entity_type): array {// phpcs:ignore Squiz.WhiteSpace.FunctionSpacing.Before
+    $fields = [];
+    if ($entity_type->id() === 'file') {
+      $fields['origname'] = BaseFieldDefinition::create('string')
+        ->setLabel($this->t('Original filename'))
+        ->setDescription($this->t('Original name of the file with no path components.'));
+    }
+
+    return $fields;
+  }
 
   /**
    * Implements hook_file_presave().
