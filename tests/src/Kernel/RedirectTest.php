348bd10527cd9b75655bd60e77cf1c357166f2c4
Stuart Clark <stu@rtclark.net>
2026-06-22 10:31:48 +0000

    test(#3269636): un-skip private:// redirect path bug
    Demonstrates that Redirect::getPath() resolves private:// destinations
    to the raw filesystem path instead of the web-accessible route, per
    https://www.drupal.org/project/filefield_paths/issues/3269636. Expected
    to fail until the underlying bug is fixed.

--- tests/src/Kernel/RedirectTest.php
+++ tests/src/Kernel/RedirectTest.php
@@ -114,11 +114,6 @@ class RedirectTest extends KernelTestBase {
   }
 
   /**
-   * Whether #3269636 has been fixed. Flip to TRUE to re-enable the test.
-   */
-  private static bool $issue3269636Fixed = FALSE;
-
-  /**
    * Tests that a private:// destination produces a web-accessible redirect.
    *
    * @see https://www.drupal.org/project/filefield_paths/issues/3269636
@@ -131,10 +126,6 @@ class RedirectTest extends KernelTestBase {
    * should start passing once #3269636 is fixed.
    */
   public function testCreateRedirectForPrivateSchemeUsesWebAccessiblePath(): void {
-    if (!self::$issue3269636Fixed) {
-      $this->markTestSkipped('Reproduces #3269636: private:// redirect resolves to filesystem path. Flip $issue3269636Fixed once fixed.');
-    }
-
     /** @var \Drupal\filefield_paths\RedirectInterface $redirect_service */
     $redirect_service = $this->container->get('filefield_paths.redirect');
 
